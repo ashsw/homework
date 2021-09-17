9b605fef4501529db6226c4fec3e853fc7bce5a4
Yile <tuyiletutu@gmail.com>
2021-09-17 14:09:49 -0400

    Initial Commit

---
+++ homework/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  homework
+//
+//  Created by TuRenhua on 2021/9/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
